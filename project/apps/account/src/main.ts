0583344e4a6e12f6bdaf73d776a6595ba9a146e6
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-12-21 16:37:37 +0700

    добавил Swagger

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -5,13 +5,28 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  //Swagger
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Account API')
+    .setDescription('The Account API description')
+    .setVersion('1.0')
+    .addTag('account')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, documentBuilder);
+
+  SwaggerModule.setup(globalPrefix, app, documentFactory);
+  //
+
+  app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
