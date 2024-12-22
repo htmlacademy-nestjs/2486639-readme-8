e1431794dbcb2bc67a0d89743cb57dca729264e3
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-12-22 18:26:49 +0700

    проработка постов

--- project/libs/shared/core/src/lib/types/post.interface.ts
+++ project/libs/shared/core/src/lib/types/post.interface.ts
@@ -1,7 +1,36 @@
 import { PostType } from './post-type.enum';
+import { User } from './user.interface';
 
-export interface Post {
+interface BasePost {
   id?: string;
   type: PostType;
-  tags: string[];
+  tags?: string[];
+  user?: User;
 }
+
+export interface VideoPost extends BasePost {
+  title: string;
+  url: string;
+}
+
+export interface TextPost extends BasePost {
+  title: string;
+  previewText: string;
+  text: string;
+}
+
+export interface QuotePost extends BasePost {
+  quote: string;
+  author: string;
+}
+
+export interface PhotoPost extends BasePost {
+  imagePath: string;
+}
+
+export interface LinkPost extends BasePost {
+  url: string;
+  text?: string;
+}
+
+export type Post = VideoPost | TextPost | QuotePost | PhotoPost | LinkPost;
