83f0b1d308835cad433fd06d5a5df7db73c2573e
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-12-22 18:32:12 +0700

    один import

--- project/libs/account/blog-user/src/blog-user.entity.ts
+++ project/libs/account/blog-user/src/blog-user.entity.ts
@@ -1,7 +1,6 @@
 import { compare, genSalt, hash } from 'bcrypt';
 
-import { Entity } from '@project/shared/core';
-import { StorableEntity, AuthUser } from '@project/shared/core';
+import { Entity, StorableEntity, AuthUser } from '@project/shared/core';
 
 import { SALT_ROUNDS } from './blog-user.constant';
 
