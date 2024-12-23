2628e4a585a25fee8dc9668574f6e493458d2bc5
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-12-23 15:54:45 +0700

    AccountConfigModule, applicationConfig, mongoConfig

--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/account-config';
+export { AccountConfigModule } from './account-config.module';
+export { applicationConfig } from './configurations/app.config';
+export { mongoConfig } from './configurations/mongo.config';
+export { getMongooseOptions } from './configurations/mongodb/get-mongoose-options';
--- project/libs/account/config/src/lib/account-config.spec.ts
+++
@@ -1,7 +0,0 @@
-import { accountConfig } from './account-config';
-
-describe('accountConfig', () => {
-  it('should work', () => {
-    expect(accountConfig()).toEqual('account-config');
-  });
-});
