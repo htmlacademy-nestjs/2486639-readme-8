4ff834eb33ede757a9446a6c04fad2b803ffb997
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-12-23 15:10:14 +0700

    Заметки, добавил @IsOptional(), но он не сработал

--- project/libs/blog/blog-post/src/dto/update-post.dto.ts
+++ project/libs/blog/blog-post/src/dto/update-post.dto.ts
@@ -1,17 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional } from "class-validator";
 
 import { PostData, PostType } from "@project/shared/core";
 
 import { PostApiProperty } from "../blog-post.constant.property";
 
 export class UpdatePostDto {
-  //@IsOptional()  //! ?
   @ApiProperty(PostApiProperty.Type)
+  @IsOptional()
   public type: PostType;
 
   @ApiProperty(PostApiProperty.Tags)
+  @IsOptional()
   public tags: string[];
 
   @ApiProperty(PostApiProperty.Data)
+  @IsOptional()
   public data: PostData;
 }
