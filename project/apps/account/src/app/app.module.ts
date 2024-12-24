71cad8078fc21968352870b74cdd8ad2fe41f8f3
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-12-24 16:24:01 +0700

    подключил и сконфигурировал модуль MongooseModule

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthenticationModule } from '@project/account/authentication'
 import { BlogUserModule } from '@project/account/blog-user'
-import { AccountConfigModule } from '@project/account/config'
+import { AccountConfigModule, getMongooseOptions } from '@project/account/config'
 
 @Module({
   imports: [
     AuthenticationModule,
     BlogUserModule,
-    AccountConfigModule
+    AccountConfigModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
   ],
   controllers: [],
   providers: []
