574693ba5570528f57f53648cb36ef89aef55a5c
Klimov Andrey <klimov.andrey.n@yandex.ru>
2024-12-24 16:37:58 +0700

    BlogUserModel, BlogUserSchema, BaseMongoRepository

---
+++ project/libs/account/blog-user/src/blog-user.model.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+import { AuthUser } from '@project/shared/core';
+
+@Schema({
+  collection: 'accounts',
+  timestamps: true
+})
+export class BlogUserModel extends Document implements AuthUser {
+  @Prop({
+    required: true,
+    unique: true
+  })
+  public email: string;
+
+  @Prop({
+    required: true
+  })
+  public login: string;
+
+  @Prop()
+  public avatrPath: string;
+
+  @Prop({
+    required: true
+  })
+  public passwordHash: string;
+}
+
+export const BlogUserSchema = SchemaFactory.createForClass(BlogUserModel);
