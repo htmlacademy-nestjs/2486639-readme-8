204139215141e7e6d27897257155ae50ed0e3848
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-01-08 16:59:51 +0700

    отладка rabbit

--- project/libs/notify/mail/src/mail.module.ts
+++ project/libs/notify/mail/src/mail.module.ts
@@ -8,7 +8,8 @@ import { MailService } from './mail.service';
 @Module({
   imports: [
     MailerModule.forRootAsync(
-      getMailerAsyncOptions())
+      getMailerAsyncOptions()
+    )
   ],
   providers: [
     MailService
