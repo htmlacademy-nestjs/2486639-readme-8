bc28f0f14add55950682fbca8e2bb958a9e6733c
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-01-10 15:15:34 +0700

    подготовка к лайкам

---
+++ project/libs/blog/blog-post-like/src/blog-post-like.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, StorableEntity, Like } from '@project/shared/core';
+
+export class BlogPostCommentEntity extends Entity implements StorableEntity<Like> {
+  public postId: string;
+  public userId: string;
+  public createdAt: Date;
+
+  constructor(like?: Like) {
+    super();
+
+    this.populate(like);
+  }
+
+  public populate(like?: Like): void {
+    if (!like) {
+      return;
+    }
+
+    this.id = like.id ?? undefined;
+    this.postId = like.postId;
+    this.userId = like.userId;
+    this.createdAt = like.createdAt ?? undefined;
+  }
+
+  public toPOJO(): Like {
+    return {
+      id: this.id,
+      postId: this.postId,
+      userId: this.userId,
+      createdAt: this.createdAt
+    }
+  }
+}
--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -16,6 +16,7 @@ export { Token } from './lib/interfaces/token.interface';
 export { AuthUser } from './lib/types/auth-user.interface';
 export { Comment } from './lib/types/comment.interface';
 export * from './lib/types/environment.type';
+export { Like } from './lib/types/like.interface';
 export { PostState } from './lib/types/post-state.enum';
 export { PostType } from './lib/types/post-type.enum';
 export { Post } from './lib/types/post.interface';
