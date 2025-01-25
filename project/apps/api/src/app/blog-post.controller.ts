be0bf0419970a46680fe222a20905ce883670594
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-01-26 02:22:00 +0700

    добавил заполнение информации о пользователе  при получении списка постов

--- project/apps/api/src/app/blog-post.controller.ts
+++ project/apps/api/src/app/blog-post.controller.ts
@@ -6,7 +6,7 @@ import { HttpService } from '@nestjs/axios';
 import { apiConfig } from '@project/api/config';
 import {
   ApiParamOption, BearerAuth, DetailPostWithUserIdRdo, DetailPostWithUserRdo, PageQuery,
-  POST_ID_PARAM, PostWithUserIdAndPaginationRdo, PostWithUserIdRdo, RequestWithRequestId,
+  POST_ID_PARAM, PostWithUserAndPaginationRdo, PostWithUserIdAndPaginationRdo, PostWithUserIdRdo, PostWithUserRdo, RequestWithRequestId,
   RequestWithRequestIdAndUserId, RouteAlias
 } from '@project/shared/core';
 import { fillDto, getQueryString, makeHeaders } from '@project/shared/helpers';
@@ -28,27 +28,29 @@ export class BlogPostController {
     private userService: UserService
   ) { }
 
-  @ApiResponse(BlogPostApiResponse.PostsFound)
+  @ApiResponse({ ...BlogPostApiResponse.PostsFound, type: PostWithUserAndPaginationRdo })
   @ApiResponse(BlogPostApiResponse.BadRequest)
   @Get('/')
-  public async index(@Query() query: SearchBlogPostQuery, @Req() { requestId }: RequestWithRequestId): Promise<PostWithUserIdAndPaginationRdo> {
+  public async index(@Query() query: SearchBlogPostQuery, @Req() { requestId }: RequestWithRequestId): Promise<PostWithUserAndPaginationRdo> {
     const url = `${this.apiOptions.blogPostServiceUrl}/${RouteAlias.Posts}${getQueryString(query)}`;
     const { data } = await this.httpService.axiosRef.get<PostWithUserIdAndPaginationRdo>(url, makeHeaders(requestId));
+    const newData = await this.userService.fillUser(data, requestId);
 
-    return data;
+    return newData;
   }
 
-  @ApiResponse(BlogPostApiResponse.SearchPosts)
+  @ApiResponse({ ...BlogPostApiResponse.SearchPosts, type: PostWithUserRdo })
   @ApiResponse(BlogPostApiResponse.BadRequest)
   @Get(`/${RouteAlias.Search}`)
-  public async find(@Query() titleQuery: TitleQuery, @Req() { requestId }: RequestWithRequestId): Promise<PostWithUserIdRdo[]> {
+  public async find(@Query() titleQuery: TitleQuery, @Req() { requestId }: RequestWithRequestId): Promise<PostWithUserRdo[]> {
     const url = `${this.apiOptions.blogPostServiceUrl}/${RouteAlias.Posts}/${RouteAlias.Search}${getQueryString(titleQuery)}`;
-    const { data } = await this.httpService.axiosRef.get<PostWithUserIdRdo[]>(url, makeHeaders(requestId));
+    const { data: posts } = await this.httpService.axiosRef.get<PostWithUserIdRdo[]>(url, makeHeaders(requestId));
+    const items = await this.userService.replaceUserIdOnUser(posts, requestId);
 
-    return data;
+    return items;
   }
 
-  @ApiResponse(BlogPostApiResponse.PostsFound)
+  @ApiResponse({ ...BlogPostApiResponse.PostsFound, type: PostWithUserAndPaginationRdo })
   @ApiResponse(BlogPostApiResponse.Unauthorized)
   @ApiBearerAuth(BearerAuth.AccessToken)
   @UseGuards(CheckAuthGuard)
@@ -56,14 +58,15 @@ export class BlogPostController {
   public async getMyPosts(
     @Query() pageQuery: PageQuery,
     @Req() { requestId, userId }: RequestWithRequestIdAndUserId
-  ): Promise<PostWithUserIdAndPaginationRdo> {
+  ): Promise<PostWithUserAndPaginationRdo> {
     const url = `${this.apiOptions.blogPostServiceUrl}/${RouteAlias.Posts}/${RouteAlias.MyPosts}${getQueryString(pageQuery)}`;
     const { data } = await this.httpService.axiosRef.get<PostWithUserIdAndPaginationRdo>(url, makeHeaders(requestId, null, userId));
+    const newData = await this.userService.fillUser(data, requestId);
 
-    return data;
+    return newData;
   }
 
-  @ApiResponse(BlogPostApiResponse.PostsFound)
+  @ApiResponse({ ...BlogPostApiResponse.PostsFound, type: PostWithUserAndPaginationRdo })
   @ApiResponse(BlogPostApiResponse.Unauthorized)
   @ApiBearerAuth(BearerAuth.AccessToken)
   @UseGuards(CheckAuthGuard)
@@ -71,11 +74,12 @@ export class BlogPostController {
   public async getMyFeed(
     @Query() query: BaseBlogPostQuery,
     @Req() { requestId, userId }: RequestWithRequestIdAndUserId
-  ): Promise<PostWithUserIdAndPaginationRdo> {
+  ): Promise<PostWithUserAndPaginationRdo> {
     const url = `${this.apiOptions.blogPostServiceUrl}/${RouteAlias.Posts}/${RouteAlias.MyFeed}${getQueryString(query)}`;
     const { data } = await this.httpService.axiosRef.get<PostWithUserIdAndPaginationRdo>(url, makeHeaders(requestId, null, userId));
+    const newData = await this.userService.fillUser(data, requestId);
 
-    return data;
+    return newData;
   }
 
   @ApiResponse({ ...BlogPostApiResponse.PostFound, type: DetailPostWithUserRdo })
